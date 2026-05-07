b110a99ff1a93e9e811e51622bcf9617d2196bfb
Sey56 <seyoumhgs@gmail.com>
2026-05-07 04:51:56 +0300

    Remove SH_Tools from tracking

--- SH_Tools/Models/CadColumn.cs
+++
@@ -1,9 +0,0 @@
-﻿using Autodesk.Revit.DB;
-namespace SH_Tools.Models
-{
-    public class CadColumn(PolyLine pLine, string message)
-    {
-        public string StatusMessage = message;
-        public PolyLine Polyline { get; set; } = pLine;
-    }
-}
