5277f45be021a6fde6483d6446e0cc7869001fb4
Sey56 <seyoumhgs@gmail.com>
2026-05-31 16:24:36 +0300

    chore: remove SH_Tools from tracking (separate project)

--- SH_Tools/Models/CadBeam.cs
+++
@@ -1,9 +0,0 @@
-﻿using Autodesk.Revit.DB;
-namespace SH_Tools.Models
-{
-    public class CadBeam(Line wLine, string message)
-    {
-        public string StatusMessage = message;
-        public Line BeamLine { get; set; } = wLine;
-    }
-}
--- SH_Tools/Models/CadWall.cs
+++
@@ -1,9 +0,0 @@
-﻿using Autodesk.Revit.DB;
-namespace SH_Tools.Models
-{
-    public class CadWall(Line wLine, string message)
-    {
-        public string StatusMessage = message;
-        public Line WallLine { get; set; } = wLine;
-    }
-}
